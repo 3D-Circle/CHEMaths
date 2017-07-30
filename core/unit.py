f10c56e81b189af3d7cc3f62bc855989b1184c43
Jingjie Yang <j.yang19@ejm.org>
2017-07-30 22:57:52 +0800

    Set the skeleton for units

---
+++ core/unit.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+"""Utility classes to represent units"""
+
+
+class Unit:
+    pass
+
+
+class MassUnit(Unit):
+    pass
+
+
+class AmountOfSubstanceUnit(Unit):
+    pass
+
+
+class VolumeUnit(Unit):
+    pass
+
+
+class EnergyUnit(Unit):
+    pass
