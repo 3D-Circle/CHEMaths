09050a7784e0fa2d381f5af212b9da0afd1dbad8
Jingjie Yang <j.yang19@ejm.org>
2017-08-07 15:04:30 +0800

    开启了伟大网页应用开发的新征程

---
+++ core/molecule.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+"""Molecule, mass and mole calculation, oxidation number, and lewis structure for organic groups."""
+from core.data_utils import get_bond_enthalpy, get_relative_atomic_mass
+from core.units import Quantity, GRAM, MOLE, GRAM_PER_MOLE, KILOJOULE_PER_MOLE
+
+ 
+class Molecule:
+    def __init__(self):
+        pass
+
+    def from_latex(self):
+        pass
+
+
+# TODO name lookup
+# TODO from ratio
+# TODO organic (lewis, etc)
