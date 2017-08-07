f5ff0ee98295c7d160059ed1567b37e9c9ea98a4
Jingjie Yang <j.yang19@ejm.org>
2017-08-07 21:04:38 +0800

    some plans

---
+++ core/solution.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+"""Chemical solution and its concentration calculation."""
+
+
+class Solution:
+    """
+    a homogeneous mixture composed of only one phase.
+    (should we support only one solute?)
+    https://en.wikipedia.org/wiki/Solution
+    """
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def from_(cls):
+        pass
+    # maybe we should include the calculation of the volume of the solution from mole, etc
+    # This has turned into something like SUVAT
+    # I hope we don't end up with equally messy code
+
+    def calculate_concentration(self):
+        pass
